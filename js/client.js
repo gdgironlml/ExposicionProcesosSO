297c465ddca28fcf6f598722c6e55f2863ed2cf0
Daniel Girón <ggironc1@miumg.edu.gt>
2026-09-11 19:16:40 -0600

    maximo de procesos modificado

--- js/client.js
+++ js/client.js
@@ -84,7 +84,7 @@ async function solicitarCrearProceso() {
 
     // C) Control de Máximo 6 Procesos Activos (Contando solo los que no han finalizado)
     const procesosActivos = Object.values(procesosGlobales).filter(p => p.estado !== 'SALIENTE');
-    if (procesosActivos.length >= 6) {
+    if (procesosActivos.length >= 4) {
         mostrarMensaje('🚫 Límite alcanzado: Ya hay 6 procesos en el sistema. Espera a que termine uno.');
         return;
     }
